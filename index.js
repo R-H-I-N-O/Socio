a95c3515b3256090d51095333fcd7d9b8dc4c236
R-H-I-N-O <sirunni@gmail.com>
2024-03-19 14:05:53 +0530

    set ejs as view engine and specify views path

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const express = require('express');
 const port = 8000;
 const router = require('./routes/index');
 
+// creating an instance of express
 const app = express();
 
+// adding routes
 app.use('/',router);
+
+// setting up view engine as ejs
+app.set('view engine', 'ejs');
+app.set('views', './views')
+
 
 app.listen(port,(err)=>{
     if(err){
