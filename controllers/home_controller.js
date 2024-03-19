c4f05758be5e8ae5b6da0a0edd03e0224d1c49ce
R-H-I-N-O <sirunni@gmail.com>
2024-03-19 13:42:24 +0530

    create routes and controller function

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home = (req,res)=>{
+    return res.end("<h1>Socio is up for testing</h1>")
+}
+
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
 const port = 8000;
+const router = require('./routes/index');
 
 const app = express();
+
+app.use('/',router);
 
 app.listen(port,(err)=>{
     if(err){
